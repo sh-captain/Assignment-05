9c01895a83ff9c7ae6996e59ea27665e5f3036d1
sh-captain <sh335629@gmail.com>
2025-08-28 04:18:24 +0600

    add new file

---
+++ script.js
@@ -0,0 +1,10 @@
+// Counts
+let hearts = 0;
+let coins = 100;
+let copies = 0;
+
+//  Navbar 
+const heartCount = document.getElementById("heartCount");
+const coinCount = document.getElementById("coinCount");
+const copyCount = document.getElementById("copyCount");
+const historyList = document.getElementById("historyList");
